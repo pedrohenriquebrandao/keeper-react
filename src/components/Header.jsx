ab230a797f7e70970d20c0b051cb7880c7fb4657
Pedro Brandão <phbrandao@outlook.com>
2022-02-12 12:58:48 -0300

    final version

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import TouchAppIcon from '@mui/icons-material/TouchApp';
+
+function Header() {
+    return (
+      <header>
+        <h1>
+          <TouchAppIcon /> Keeper
+        </h1>
+      </header>
+    );
+  }
+  
+export default Header;
